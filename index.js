eda3c1ca363c8147493225b17407c45853aee3fc
jaumard <ioquoo3H>
2015-12-19 23:34:17 +0100

    Fix missing test Fix eslint errors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const Trailpack = require('trailpack')
-const lib = require('./lib')
+//const lib = require('./lib')
 const _ = require('lodash')
 const webpack = require('webpack')
 
@@ -19,13 +19,13 @@ module.exports = class Webpack extends Trailpack {
      * Ensure that config/webpack is valid
      */
     validate() {
-	    let config = this.app.config
-	    const logger = this.app.config.log.logger
+        const config = this.app.config
+        const logger = this.app.config.log.logger
 
-	    if (!config.webpack.options) {
-		    logger.warn('trailspack-webpack: no Webpack "options" are defined.')
-		    logger.warn('trailspack-webpack: Please configure config/webpack.js')
-	    }
+        if (!config.webpack.options) {
+            logger.warn('trailspack-webpack: no Webpack "options" are defined.')
+            logger.warn('trailspack-webpack: Please configure config/webpack.js')
+        }
         return Promise.resolve()
     }
 
@@ -33,45 +33,45 @@ module.exports = class Webpack extends Trailpack {
      * Start Webpack
      */
     initialize() {
-	    const logger = this.app.config.log.logger
-	    return new Promise((resolve, reject) => {
-		    this.compiler = webpack(this.app.config.webpack.options, (err, stats) => {
-			    if (err) return reject(err)
+        const logger = this.app.config.log.logger
+        return new Promise((resolve, reject) => {
+            this.compiler = webpack(this.app.config.webpack.options, (err, stats) => {
+                if (err) return reject(err)
 
-			    logger.info('trailspack-webpack: compiler loaded.')
-			    logger.silly('trailspack-webpack: ', stats.toString())
+                logger.info('trailspack-webpack: compiler loaded.')
+                logger.silly('trailspack-webpack: ', stats.toString())
 
-			    if (process.env.NODE_ENV == 'development') {
-				    logger.info('trailspack-webpack: watching...')
-				    this.compiler.watch(_.extend({ }, this.app.config.webpack.watchOptions), this.afterBuild.bind(this))
-			    }
-			    else {
-				    logger.info('trailspack-webpack: running...')
-				    this.compiler.run(this.afterBuild.bind(this))
-			    }
-			    resolve()
-		    })
-	    })
+                if (process.env.NODE_ENV == 'development') {
+                    logger.info('trailspack-webpack: watching...')
+                    this.compiler.watch(_.extend({}, this.app.config.webpack.watchOptions), this.afterBuild.bind(this))
+                }
+                else {
+                    logger.info('trailspack-webpack: running...')
+                    this.compiler.run(this.afterBuild.bind(this))
+                }
+                resolve()
+            })
+        })
     }
 
-	afterBuild (err, rawStats) {
-		const logger = this.app.config.log.logger
-		if (err) return logger.error('trailspack-webpack: FATAL ERROR', err)
+    afterBuild(err, rawStats) {
+        const logger = this.app.config.log.logger
+        if (err) return logger.error('trailspack-webpack: FATAL ERROR', err)
 
-		let stats = rawStats.toJson()
+        const stats = rawStats.toJson()
 
-		logger.debug('trailspack-webpack: Build Info\n' + rawStats.toString({
-				colors: true,
-				chunks: false
-			}))
+        logger.debug('trailspack-webpack: Build Info\n' + rawStats.toString({
+                colors: true,
+                chunks: false
+            }))
 
-		if (stats.errors.length > 0) {
-			logger.error('trailspack-webpack:', stats.errors)
-		}
-		if (stats.warnings.length > 0) {
-			logger.warn('trailspack-webpack:', stats.warnings)
-		}
-	}
+        if (stats.errors.length > 0) {
+            logger.error('trailspack-webpack:', stats.errors)
+        }
+        if (stats.warnings.length > 0) {
+            logger.warn('trailspack-webpack:', stats.warnings)
+        }
+    }
 
     constructor(app, config) {
         super(app, {
